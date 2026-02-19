5949b2621d21bfd34b3d98a020f3279f2e8ef340
eva <eva.z@frc1073.org>
2026-02-19 00:32:14 -0500

    Updated HubFinder coordinates

--- src/main/cpp/subsystems/ZoneFinder.cpp
+++ src/main/cpp/subsystems/ZoneFinder.cpp
@@ -2,19 +2,35 @@
 
 //AndyMark Dimentions
 const frc::Rectangle2d ZoneFinder::BLUEZONE = frc::Rectangle2d(frc::Translation2d(0_in, 0_in), frc::Translation2d(181.56_in, 316.64_in));
-
 const frc::Rectangle2d ZoneFinder::REDZONE = frc::Rectangle2d(frc::Translation2d(468.56_in, 0_in), frc::Translation2d(650.12_in, 316.64_in));
 const frc::Rectangle2d ZoneFinder::NEUTRALZONE = frc::Rectangle2d(frc::Translation2d(181.56_in, 0_in), frc::Translation2d(468.56_in, 316.64_in));
 
 const frc::Rectangle2d ZoneFinder::TRENCH_A = frc::Rectangle2d(frc::Translation2d(156.06_in, 0_in), frc::Translation2d(200.46_in, 49.86_in));
 const frc::Rectangle2d ZoneFinder::TRENCH_B = frc::Rectangle2d(frc::Translation2d(156.06_in, 266.78_in), frc::Translation2d(200.46_in, 316.64_in));
-const frc::Rectangle2d ZoneFinder::TRENCH_C = frc::Rectangle2d(frc::Translation2d(445.06_in, 0_in), frc::Translation2d(0_in, 0_in));
-const frc::Rectangle2d ZoneFinder::TRENCH_D = frc::Rectangle2d(frc::Translation2d(445.06_in, 0_in), frc::Translation2d(0_in, 0_in));
+const frc::Rectangle2d ZoneFinder::TRENCH_C = frc::Rectangle2d(frc::Translation2d(445.06_in, 0_in), frc::Translation2d(489.46_in, 49.86_in));
+const frc::Rectangle2d ZoneFinder::TRENCH_D = frc::Rectangle2d(frc::Translation2d(445.06_in, 266.78_in), frc::Translation2d(489.46_in, 316.64_in));
 
 const frc::Rectangle2d ZoneFinder::BUMP_A = frc::Rectangle2d(frc::Translation2d(156.06_in, 61.86_in), frc::Translation2d(200.46_in, 134.86_in));
 const frc::Rectangle2d ZoneFinder::BUMP_B = frc::Rectangle2d(frc::Translation2d(156.06_in, 181.78_in), frc::Translation2d(200.46_in, 254.78_in));
-const frc::Rectangle2d ZoneFinder::BUMP_C = frc::Rectangle2d(frc::Translation2d(445.06_in, 0_in), frc::Translation2d(0_in, 0_in));
-const frc::Rectangle2d ZoneFinder::BUMP_D = frc::Rectangle2d(frc::Translation2d(445.06_in, 0_in), frc::Translation2d(0_in, 0_in));
+const frc::Rectangle2d ZoneFinder::BUMP_C = frc::Rectangle2d(frc::Translation2d(445.06_in, 61.86_in), frc::Translation2d(489.46_in, 134.86_in));
+const frc::Rectangle2d ZoneFinder::BUMP_D = frc::Rectangle2d(frc::Translation2d(445.06_in, 181.78_in), frc::Translation2d(489.46_in, 254.78_in));
+
+//Welded Dimentions
+/*
+const frc::Rectangle2d ZoneFinder::BLUEZONE = frc::Rectangle2d(frc::Translation2d(0_in, 0_in), frc::Translation2d(156.61_in, 317.69_in));
+const frc::Rectangle2d ZoneFinder::REDZONE = frc::Rectangle2d(frc::Translation2d(490.01_in, 0_in), frc::Translation2d(651.22_in, 317.69_in));
+const frc::Rectangle2d ZoneFinder::NEUTRALZONE = frc::Rectangle2d(frc::Translation2d(201.01_in, 0_in), frc::Translation2d(325.61_in, 317.69_in));
+
+const frc::Rectangle2d ZoneFinder::TRENCH_A = frc::Rectangle2d(frc::Translation2d(156.61_in, 0_in), frc::Translation2d(201.01_in, 50.35_in));
+const frc::Rectangle2d ZoneFinder::TRENCH_B = frc::Rectangle2d(frc::Translation2d(156.61_in, 267.098_in), frc::Translation2d(201.01_in, 317.69_in));
+const frc::Rectangle2d ZoneFinder::TRENCH_C = frc::Rectangle2d(frc::Translation2d(445.61_in, 0_in), frc::Translation2d(490.01_in, 50.35_in));
+const frc::Rectangle2d ZoneFinder::TRENCH_D = frc::Rectangle2d(frc::Translation2d(445.61_in, 267.098_in), frc::Translation2d(490.01_in, 317.69_in));
+
+const frc::Rectangle2d ZoneFinder::BUMP_A = frc::Rectangle2d(frc::Translation2d(156.61_in, 62.59_in), frc::Translation2d(201.01_in, 135.59_in));
+const frc::Rectangle2d ZoneFinder::BUMP_B = frc::Rectangle2d(frc::Translation2d(156.61_in, 182.1_in), frc::Translation2d(201.01_in, 255.1_in));
+const frc::Rectangle2d ZoneFinder::BUMP_C = frc::Rectangle2d(frc::Translation2d(445.61_in, 62.59_in), frc::Translation2d(490.01_in, 135.59_in));
+const frc::Rectangle2d ZoneFinder::BUMP_D = frc::Rectangle2d(frc::Translation2d(445.61_in, 182.1_in), frc::Translation2d(490.01_in, 255.1_in));
+*/
 
 ZoneFinder::ZoneFinder(std::shared_ptr<Localizer> _localizer)
 {
