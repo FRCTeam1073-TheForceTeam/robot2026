7624c34201eda0c8ef58fe66ae30418d294060a8
eva <eva.z@frc1073.org>
2026-02-24 20:35:22 -0500

    Created periodic, updateAlliance, and turretPos

--- src/main/include/subsystems/HubFinder.h
+++ src/main/include/subsystems/HubFinder.h
@@ -4,10 +4,10 @@
 #include <frc/geometry/Translation2d.h>
 #include <frc/geometry/Rotation2d.h>
 #include <units/length.h>
+#include <frc/geometry/Rotation2d.h>
+
 
 #include "subsystems/Localizer.h"
-
-
 
 
 
@@ -15,17 +15,26 @@ class HubFinder  : public frc2::SubsystemBase {
     public:
     static const frc::Pose2d REDHUB;
     static const frc::Pose2d BLUEHUB;
+    static const frc::Transform2d ROBOTOTURRET;
     
     std::shared_ptr<Localizer> _localizer;
     HubFinder(std::shared_ptr<Localizer> localizer);
 
-    frc::Pose2d HubLoc;
-    
+    void Periodic() override;
+
     frc::Pose2d getHubPos();
+    units::angle::radian_t position;
+    units::angle::radian_t getTurretToHubAngle();
 
     private:
-    static std::optional<frc::DriverStation::Alliance> _alliance;
+    void UpdateAlliance();
+    
+    std::optional<frc::DriverStation::Alliance> _alliance;
+
+    frc::Pose2d HubLoc;
+    frc::Pose2d TurretLoc;
     frc::Pose2d OurHub;
     frc::Pose2d RoboPos;
+    frc::Rotation2d RoboRotation;
 
 };
