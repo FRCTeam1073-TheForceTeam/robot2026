939b76af37bd7b946535eaa60a39ebfe7387dfe3
roboticsgeek <rtpack@charter.net>
2026-03-05 16:09:08 -0500

    Update SwerveControlConfig.cpp

--- src/main/cpp/config/SwerveControlConfig.cpp
+++ src/main/cpp/config/SwerveControlConfig.cpp
@@ -4,7 +4,7 @@
 
 ctre::phoenix6::configs::Slot0Configs SwerveControlConfig::GetDriveControlConfig() {
     ctre::phoenix6::configs::Slot0Configs config;
-    config.kV = 0.12;
+    config.kV = 0.12;   // kV for X60
     config.kP = 0.4;
     config.kI = 0.0;
     config.kD = 0.0;
@@ -25,29 +25,3 @@ ctre::phoenix6::configs::Slot0Configs SwerveControlConfig::GetSteerControlConfig
 
     return config;
 }
-
-
-    // Historical reference:
-    // const double gearRatio = 6.75f;
-    // const double wheelDiameterMeters = 0.1016f;
-    // const double rotationsPerMeter = gearRatio/(wheelDiameterMeters * 1.06 * M_PI); // 1.06 is the measure correction factor while driving
-
-
-    // const double radiansPerRotation = (150/7) / (2.0 * M_PI);
-    // const double steerCurrentLimit = 20;
-    // const double driveCurrentLimit = 35;
-    // const double steerCurrentThreshold = 12;
-    // const double driveCurrentThreshold = 22;
-    // const double steerCurrentThresholdTime = 0.1;
-    // const double driveCurrentThresholdTime = 0.25;
-    // double steerP;
-    // double steerI;
-    // double steerD;
-    // double steerV;
-    // double driveP;
-    // double driveI;
-    // double driveD;
-    // double driveV;
-    // double driveA;
-    // const double driveMaxIntegrator = 400.0;
-    // const double steerMaxIntegrator = 400.0;
