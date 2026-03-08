42a9d70e319108284625222d8877e761338f82b1
roboticsgeek <rtpack@charter.net>
2026-03-08 10:52:25 -0400

    Drivetrain tuning update.

--- src/main/cpp/config/SwerveControlConfig.cpp
+++ src/main/cpp/config/SwerveControlConfig.cpp
@@ -5,11 +5,11 @@
 ctre::phoenix6::configs::Slot0Configs SwerveControlConfig::GetDriveControlConfig() {
     ctre::phoenix6::configs::Slot0Configs config;
     config.kV = 0.12;   // kV for X60
-    config.kP = 0.3;
+    config.kP = 0.35;
     config.kI = 0.0;
     config.kD = 0.0;
     config.kA = 0.0;
-    config.kS = 0.02;
+    config.kS = 0.015;
 
     return config;
 }
@@ -18,8 +18,8 @@ ctre::phoenix6::configs::Slot0Configs SwerveControlConfig::GetSteerControlConfig
     ctre::phoenix6::configs::Slot0Configs config;
     config.kV = 0.153;     // kV for X44
     config.kP = 11.0;
-    config.kI = 0.45;
-    config.kD = 0.02;
+    config.kI = 0.8;
+    config.kD = 0.04;
     config.kA = 0.0;
     config.kS = 0.04;
 
