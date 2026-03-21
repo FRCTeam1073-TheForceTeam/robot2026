f5c8b96628268e72ae9467fbb4d4585f1b89a02e
Allen <allen.g@frc1073.org>
2026-03-21 13:05:01 -0400

    added ballistic shot (it is not used yet)

---
+++ src/main/cpp/utilities/BallisticShot.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "utilities/BallisticShot.h"
+
+BallisticShot::BallisticShot() = default;
+
+BallisticShot::Shot BallisticShot::GetShot(units::length::meter_t range, units::length::meter_t heightAboveHub){
+    // hub is 6 feet tall or 1.829 meters
+    maxHeight = heightAboveHub + hubHeight;
+    yVel = std::sqrt(((maxHeight - turretHeight) * 2 * gravity).value()) * 1_mps;
+    timeToMaxHeight = yVel / gravity;
+    timeToFall = std::sqrt((2 * heightAboveHub / gravity).value()) * 1_s;
+    xVel = range / (timeToMaxHeight + timeToFall);
+    hoodAngle = atan2(yVel.value(), xVel.value()) * 1_rad;
+    flywheelSpeed = std::sqrt((xVel * xVel + yVel * yVel).value()) * 1_mps;
+    shot.FlywheelSpeed = flywheelSpeed;
+    shot.HoodAngle = hoodAngle;
+    return shot;
+}
