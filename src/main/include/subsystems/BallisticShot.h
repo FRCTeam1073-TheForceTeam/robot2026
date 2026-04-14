04f2012be4f4dfabbdc3f50060565c51dd6205a4
Logan-Perry <logan.p@frc1073.org>
2026-04-13 20:17:56 -0400

    Updated Efficiency

--- src/main/include/subsystems/BallisticShot.h
+++ src/main/include/subsystems/BallisticShot.h
@@ -45,7 +45,7 @@ class BallisticShot : public frc2::SubsystemBase {
   static constexpr units::length::meter_t heightAboveHub = 1.0_m;
   static constexpr units::length::meter_t hubHeight = 1.829_m;
   static constexpr units::length::meter_t turretHeight = 0.40_m;
-  static constexpr double efficiency = 0.80;   // Calibration for transfer of flywheel velocity to fuel.
+  static constexpr double efficiency = 0.83;   // Calibration for transfer of flywheel velocity to fuel.
   // static constexpr units::angle::radian_t hood_offset = -0.12_rad; // Calibration for shot exit vs. hood angle.
   static constexpr units::angle::radian_t hood_offset = -0.14_rad; // Calibration for shot exit vs. hood angle.
 
