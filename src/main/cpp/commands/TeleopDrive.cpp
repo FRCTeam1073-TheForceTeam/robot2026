35382dad595fc0dd1370bf7f0c4cce8e6d1a39e4
Arjun <angelina.cos.33@gmail.com>
2026-04-17 15:59:59 -0400

    moved bump points on left and right autos

--- src/main/cpp/commands/TeleopDrive.cpp
+++ src/main/cpp/commands/TeleopDrive.cpp
@@ -23,12 +23,13 @@ TeleopDrive::TeleopDrive(std::shared_ptr<Drivetrain>& drivetrain, std::shared_pt
     // parked = false;
     last_error = 0;
     last_snap_time = 0;
+    
     angle_tolerance = 0.05_rad;
     torqueGate = 65_N;
     slowMode = false;
     lastYPressed = false;
     lastXPressed = false;
-    fastRotation = false;
+    fastRotation = true;
     // TODO: chassisspeeds and speeds appear in the java drivetrain; determine if these are necessary for the c++ file
     // TODO: pointAtTarget boolean, localizer, lidar and aprilTagFinder appears in the java drivetrain, but it might be a better idea to put these in the localize file
 
